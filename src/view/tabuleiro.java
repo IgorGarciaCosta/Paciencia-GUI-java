26fe54a25bfcad7b53771dcae5d143f318333098
IgorGarciaCosta <igonildo7@gmail.com>
2021-05-10 23:06:10 -0300

    Movimentação das cartas
    Adição de janelas com opções de mover cartas ao clicá-las.

--- src/view/tabuleiro.java
+++ src/view/tabuleiro.java
@@ -17,6 +17,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Panel;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.Image;
@@ -28,7 +31,6 @@ public class tabuleiro {
 	final static Partida partida = new Partida();
 	Paciencia paciencia = partida.retornaPacienciaAtual();
 	MonteDeCartas pilhaRecebida = new MonteDeCartas();
-	
 
 	private JFrame frame;
 
@@ -206,99 +208,97 @@ public class tabuleiro {
 
 	public void insereQuantDeCartas() {
 		int tab1 = paciencia.getMonteFileira(0).size();
-		JLabel t1Number = new JLabel("("+tab1+")");
+		JLabel t1Number = new JLabel("(" + tab1 + ")");
 		t1Number.setForeground(Color.WHITE);
 		t1Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		t1Number.setBounds(45, 235, 17, 14);
 		frame.getContentPane().add(t1Number);
 
 		int tab2 = paciencia.getMonteFileira(1).size();
-		JLabel t2Number = new JLabel("("+tab2+")");
+		JLabel t2Number = new JLabel("(" + tab2 + ")");
 		t2Number.setForeground(Color.WHITE);
 		t2Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		t2Number.setBounds(106, 235, 17, 14);
 		frame.getContentPane().add(t2Number);
 
 		int tab3 = paciencia.getMonteFileira(2).size();
-		JLabel t3Number = new JLabel("("+tab3+")");
+		JLabel t3Number = new JLabel("(" + tab3 + ")");
 		t3Number.setForeground(Color.WHITE);
 		t3Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		t3Number.setBounds(165, 235, 17, 14);
 		frame.getContentPane().add(t3Number);
 
 		int tab4 = paciencia.getMonteFileira(3).size();
-		JLabel t4Number = new JLabel("("+tab4+")");
+		JLabel t4Number = new JLabel("(" + tab4 + ")");
 		t4Number.setForeground(Color.WHITE);
 		t4Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		t4Number.setBounds(219, 235, 17, 14);
 		frame.getContentPane().add(t4Number);
 
 		int tab5 = paciencia.getMonteFileira(4).size();
-		JLabel t5Number = new JLabel("("+tab5+")");
+		JLabel t5Number = new JLabel("(" + tab5 + ")");
 		t5Number.setForeground(Color.WHITE);
 		t5Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		t5Number.setBounds(281, 235, 17, 14);
 		frame.getContentPane().add(t5Number);
 
 		int tab6 = paciencia.getMonteFileira(5).size();
-		JLabel t6Number = new JLabel("("+tab6+")");
+		JLabel t6Number = new JLabel("(" + tab6 + ")");
 		t6Number.setForeground(Color.WHITE);
 		t6Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		t6Number.setBounds(339, 235, 17, 14);
 		frame.getContentPane().add(t6Number);
 
 		int tab7 = paciencia.getMonteFileira(6).size();
-		JLabel t7Number = new JLabel("("+tab7+")");
+		JLabel t7Number = new JLabel("(" + tab7 + ")");
 		t7Number.setForeground(Color.WHITE);
 		t7Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		t7Number.setBounds(396, 235, 17, 14);
 		frame.getContentPane().add(t7Number);
-		
+
 		int est = paciencia.getMonteEstoque().size();
-		JLabel estNumber = new JLabel("("+est+")");
+		JLabel estNumber = new JLabel("(" + est + ")");
 		estNumber.setForeground(Color.WHITE);
 		estNumber.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		estNumber.setBounds(39, 33, 17, 14);
 		frame.getContentPane().add(estNumber);
-		
+
 		int desc = paciencia.getMonteDescarte().size();
-		JLabel descNumber = new JLabel("("+desc+")");
+		JLabel descNumber = new JLabel("(" + desc + ")");
 		descNumber.setForeground(Color.WHITE);
 		descNumber.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		descNumber.setBounds(106, 33, 17, 14);
 		frame.getContentPane().add(descNumber);
-		
+
 		int f1 = paciencia.getMonteFundacao(0).size();
-		JLabel f1Number = new JLabel("("+f1+")");
+		JLabel f1Number = new JLabel("(" + f1 + ")");
 		f1Number.setForeground(Color.WHITE);
 		f1Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		f1Number.setBounds(246, 34, 17, 14);
 		frame.getContentPane().add(f1Number);
-		
+
 		int f2 = paciencia.getMonteFundacao(1).size();
-		JLabel f2Number = new JLabel("("+f2+")");
+		JLabel f2Number = new JLabel("(" + f2 + ")");
 		f2Number.setForeground(Color.WHITE);
 		f2Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		f2Number.setBounds(305, 33, 17, 14);
 		frame.getContentPane().add(f2Number);
-		
+
 		int f3 = paciencia.getMonteFundacao(2).size();
-		JLabel f3Number = new JLabel("("+f3+")");
+		JLabel f3Number = new JLabel("(" + f3 + ")");
 		f3Number.setForeground(Color.WHITE);
 		f3Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		f3Number.setBounds(361, 33, 17, 14);
 		frame.getContentPane().add(f3Number);
-		
+
 		int f4 = paciencia.getMonteFundacao(3).size();
-		JLabel f4Number = new JLabel("("+f4+")");
+		JLabel f4Number = new JLabel("(" + f4 + ")");
 		f4Number.setForeground(Color.WHITE);
 		f4Number.setFont(new Font("Tahoma", Font.PLAIN, 7));
 		f4Number.setBounds(417, 34, 17, 14);
 		frame.getContentPane().add(f4Number);
 	}
 
-	
-	
 	public void colocaDescarteNaTela() {
 		pilhaRecebida = paciencia.getMonteDescarte();
 		Carta carta = pilhaRecebida.visualizarCartaDoTopo();
@@ -310,6 +310,7 @@ public class tabuleiro {
 			desc.setIcon(new ImageIcon(newImg));
 			desc.setBounds(72, 58, 46, 64);
 			frame.getContentPane().add(desc);
+			perguntaPraOndeMover(desc, carta, "desc");
 		}
 
 	}
@@ -324,6 +325,7 @@ public class tabuleiro {
 			est.setIcon(new ImageIcon(newImg));
 			est.setBounds(10, 58, 46, 64);
 			frame.getContentPane().add(est);
+			perguntaPraOndeMover(est, carta, "est");
 		}
 
 	}
@@ -340,6 +342,7 @@ public class tabuleiro {
 			fund1.setIcon(new ImageIcon(newImg));
 			fund1.setBounds(378, 58, 46, 64);
 			frame.getContentPane().add(fund1);
+			perguntaPraOndeMover(fund1, carta, "f1");
 		}
 
 		pilhaRecebida = paciencia.getMonteFundacao(1);
@@ -351,6 +354,7 @@ public class tabuleiro {
 			fund2.setIcon(new ImageIcon(newImg));
 			fund2.setBounds(322, 59, 46, 64);
 			frame.getContentPane().add(fund2);
+			perguntaPraOndeMover(fund2, carta, "f2");
 		}
 
 		pilhaRecebida = paciencia.getMonteFundacao(2);
@@ -362,6 +366,7 @@ public class tabuleiro {
 			fund3.setIcon(new ImageIcon(newImg));
 			fund3.setBounds(266, 59, 46, 64);
 			frame.getContentPane().add(fund3);
+			perguntaPraOndeMover(fund3, carta, "f3");
 		}
 
 		pilhaRecebida = paciencia.getMonteFundacao(3);
@@ -373,12 +378,12 @@ public class tabuleiro {
 			fund4.setIcon(new ImageIcon(newImg));
 			fund4.setBounds(210, 59, 46, 64);
 			frame.getContentPane().add(fund4);
+			perguntaPraOndeMover(fund4, carta, "f4");
 		}
 
 	}
 
 	public void colocaFileirasNaTela() {
-
 		pilhaRecebida = paciencia.getMonteFileira(0);
 		Carta carta = pilhaRecebida.visualizarCartaDoTopo();
 		Image img = new ImageIcon(this.getClass().getResource("/cards/cardBack.png")).getImage();
@@ -388,6 +393,7 @@ public class tabuleiro {
 		tab1.setIcon(new ImageIcon(newImg));
 		tab1.setBounds(10, 171, 46, 64);
 		frame.getContentPane().add(tab1);
+		perguntaPraOndeMover(tab1, carta, "t1");
 
 		pilhaRecebida = paciencia.getMonteFileira(1);
 		carta = pilhaRecebida.visualizarCartaDoTopo();
@@ -397,6 +403,7 @@ public class tabuleiro {
 		tab2.setIcon(new ImageIcon(newImg));
 		tab2.setBounds(72, 171, 46, 64);
 		frame.getContentPane().add(tab2);
+		perguntaPraOndeMover(tab2, carta, "t2");
 
 		pilhaRecebida = paciencia.getMonteFileira(2);
 		carta = pilhaRecebida.visualizarCartaDoTopo();
@@ -406,6 +413,7 @@ public class tabuleiro {
 		tab3.setIcon(new ImageIcon(newImg));
 		tab3.setBounds(131, 171, 46, 64);
 		frame.getContentPane().add(tab3);
+		perguntaPraOndeMover(tab3, carta, "t3");
 
 		pilhaRecebida = paciencia.getMonteFileira(3);
 		carta = pilhaRecebida.visualizarCartaDoTopo();
@@ -415,6 +423,7 @@ public class tabuleiro {
 		tab4.setIcon(new ImageIcon(newImg));
 		tab4.setBounds(187, 171, 46, 64);
 		frame.getContentPane().add(tab4);
+		perguntaPraOndeMover(tab4, carta, "t4");
 
 		pilhaRecebida = paciencia.getMonteFileira(4);
 		carta = pilhaRecebida.visualizarCartaDoTopo();
@@ -424,6 +433,7 @@ public class tabuleiro {
 		tab5.setIcon(new ImageIcon(newImg));
 		tab5.setBounds(246, 171, 46, 64);
 		frame.getContentPane().add(tab5);
+		perguntaPraOndeMover(tab5, carta, "t5");
 
 		pilhaRecebida = paciencia.getMonteFileira(5);
 		carta = pilhaRecebida.visualizarCartaDoTopo();
@@ -433,6 +443,7 @@ public class tabuleiro {
 		tab6.setIcon(new ImageIcon(newImg));
 		tab6.setBounds(308, 171, 46, 64);
 		frame.getContentPane().add(tab6);
+		perguntaPraOndeMover(tab6, carta, "t6");
 
 		pilhaRecebida = paciencia.getMonteFileira(6);
 		carta = pilhaRecebida.visualizarCartaDoTopo();
@@ -442,6 +453,7 @@ public class tabuleiro {
 		tab7.setIcon(new ImageIcon(newImg));
 		tab7.setBounds(364, 171, 46, 64);
 		frame.getContentPane().add(tab7);
+		perguntaPraOndeMover(tab7, carta, "t7");
 
 	}
 
@@ -664,5 +676,76 @@ public class tabuleiro {
 		}
 
 		return img;
+	}
+
+	public void perguntaPraOndeMover(JLabel labelDaCarta, Carta carta, String pilhaClicada) {
+		labelDaCarta.addMouseListener((MouseListener) new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				String[] values = {};
+				Object selected = null;
+				switch (pilhaClicada) {
+				case "est":
+
+					String[] values1 = { "Descarte" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values1, "0");
+					break;
+				case "desc":
+					String[] values2 = { "Fundação 1", "Fundação 2", "Fundação 3", "Fundação 4", "Fileira 1",
+							"Fileira 2", "Fileira 3", "Fileira 4", "Fileira 5", "Fileira 6", "Fileira 7" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values2, "0");
+					break;
+
+				case "t1":
+					String[] values3 = { "Fundação 1", "Fundação 2", "Fundação 3", "Fundação 4", "Fileira 2",
+							"Fileira 3", "Fileira 4", "Fileira 5", "Fileira 6", "Fileira 7" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values3, "0");
+					break;
+				case "t2":
+					String[] values4 = { "Fundação 1", "Fundação 2", "Fundação 3", "Fundação 4", "Fileira 1",
+							"Fileira 3", "Fileira 4", "Fileira 5", "Fileira 6", "Fileira 7" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values4, "0");
+					break;
+				case "t3":
+					String[] values5 = { "Fundação 1", "Fundação 2", "Fundação 3", "Fundação 4", "Fileira 1",
+							"Fileira 2", "Fileira 4", "Fileira 5", "Fileira 6", "Fileira 7" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values5, "0");
+					break;
+				case "t4":
+					String[] values6 = { "Fundação 1", "Fundação 2", "Fundação 3", "Fundação 4", "Fileira 1",
+							"Fileira 2", "Fileira 3", "Fileira 5", "Fileira 6", "Fileira 7" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values6, "0");
+					break;
+				case "t5":
+					String[] values7 = { "Fundação 1", "Fundação 2", "Fundação 3", "Fundação 4", "Fileira 1",
+							"Fileira 2", "Fileira 3", "Fileira 4", "Fileira 6", "Fileira 7" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values7, "0");
+					break;
+				case "t6":
+					String[] values8 = { "Fundação 1", "Fundação 2", "Fundação 3", "Fundação 4", "Fileira 1",
+							"Fileira 2", "Fileira 3", "Fileira 4", "Fileira 5", "Fileira 7" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values8, "0");
+					break;
+				case "t7":
+					String[] values9 = { "Fundação 1", "Fundação 2", "Fundação 3", "Fundação 4", "Fileira 1",
+							"Fileira 2", "Fileira 3", "Fileira 4", "Fileira 5", "Fileira 6" };
+					selected = JOptionPane.showInputDialog(null, "Para onde enviar esta carta?", "Movimentação",
+							JOptionPane.DEFAULT_OPTION, null, values9, "0");
+					break;
+				}
+
+				if (selected != null) {// null if the user cancels.
+					// mover carta
+					System.out.print(selected);
+				}
+			}
+		});
 	}
 }
